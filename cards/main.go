99767c042fa77f10b8c66a6f347f4838fa01b793
Loc Truong <loc@kiuglobal.com>
2018-01-12 01:09:22 +0700

    strings Join

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+	fmt.Println(cards.toString())
 }
