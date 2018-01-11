c049bf4ea4cf2213ff6181269e3e2be035b49481
Loc Truong <loc@kiuglobal.com>
2018-01-11 23:56:24 +0700

    slice and for loop

--- cards/main.go
+++ cards/main.go
@@ -5,9 +5,12 @@ import (
 )
 
 func main() {
-	card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
